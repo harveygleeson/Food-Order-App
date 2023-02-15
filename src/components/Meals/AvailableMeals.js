833d3fa16c0cdb875321c11d8842a44c067edefd
Harvey Gleeson <harveygleeson@gmail.com>
2023-02-15 13:00:58 +0000

    Add input, modal and dummy cart

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,7 +31,7 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem meal={meal} key={meal.id} />
+    <MealItem meal={meal} key={meal.id} id={meal.id} />
   ));
 
   return (
